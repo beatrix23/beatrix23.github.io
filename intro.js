d9a0487ba1dd29dd451251c4c2309a021d951208
beatrixs <beatrixsocaci@gmail.com>
2019-03-11 21:00:05 +0200

    cleanup js intro

--- intro.js
+++ intro.js
@@ -1,19 +1,3 @@
-console.info("my first js file")
-
-var name = "Beatrix";
-var employed = false;
-
-console.warn("employed");
-console.warn(employed);
-
-console.info("Name:");
-console.info(name);
-
-name = "Bea";
-
-console.info("Name:");
-console.info(name);
-
 function getWelcomeMsg() {
     return "Wellcome to my page!";
 }
@@ -38,47 +22,11 @@ console.info("summaryElement", summaryElement);
 var color = getColor();
 summaryElement.style.color = color;
 
-//first example
-function hideHomePage() {
-    document.getElementById('home-page').style.display = 'none';
-}
- 
-function showSkillsPage() {
-    document.getElementById('skills-page').style.display = 'block';
-}
-function clickOnHome() {
-    hidePage('skills-page');
-    hidePage('languages-page');
-    hidePage('about-page');
-    showPage('home-page');
- }
 
-function clickOnSkills() {
-   // hideHomePage();
-   // showSkillsPage();
-   hidePage('home-page');
-   hidePage('languages-page');
-   hidePage('about-page');
-   showPage('skills-page');
-}
-
-function clickOnLanguages() {
-    hidePage('home-page');
-    hidePage('about-page');
-    hidePage('skills-page');
-    showPage('languages-page');
-}
-
-function clickOnAbout () {
-    hidePage('home-page');
-    hidePage('skills-page');
-    hidePage('languages-page');
-    showPage('about-page');
-}
-//first exemple-end
 
 function hidePage(page) {
-    document.getElementById(page).style.display = 'none';
+    var el = document.getElementById(page);
+    el.style,display = 'none' ;
 }
  
 function showPage(page) {
