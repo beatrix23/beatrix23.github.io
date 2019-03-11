c2a11a1d43a0a43ed44b7ebcf2e9079ace243e0d
beatrixs <beatrixsocaci@gmail.com>
2019-03-11 22:03:20 +0200

    Schimbare

--- intro.js
+++ intro.js
@@ -13,7 +13,6 @@ function getColor() {
 
 }
 
-
 var wellcome = getWelcomeMsg();
 console.info(wellcome);
 
@@ -21,8 +20,6 @@ var summaryElement = document.getElementById("summary");
 console.info("summaryElement", summaryElement);
 var color = getColor();
 summaryElement.style.color = color;
-
-
 
 function hidePage(page) {
     var el = document.getElementById(page);
@@ -32,3 +29,30 @@ function hidePage(page) {
 function showPage(page) {
     document.getElementById(page).style.display = 'block';
 }
+
+function initMenu() {
+    var links = document.querySelectorAll("#top-menu-bar a");
+    console.info(links);
+    for(var i = 0; i < links.length; i++) {
+        links[i].onclick = clickOnMenuItem;
+
+    }
+}
+
+function clickOnMenuItem () {
+    console.warn('click on menu', this);
+    hideAllPages();
+    var pageId = this.getAttribute('data-page');
+    console.warn({pageId});
+    showPage(pageId);
+
+}
+
+function hideAllPages() {
+    var pages = document.querySelectorAll(".page");
+    for (var i = 0; i < pages.length; i++ ) {
+        pages[i].style.display = 'none';
+    }
+}
+
+initMenu();
